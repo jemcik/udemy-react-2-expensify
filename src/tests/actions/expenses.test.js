a32b41111e7ae9d20778ae87b7a915f8f58885c3
Ievgen Iemchynskyi <iiemchynskyi@conductor.com>
2018-03-22 17:21:33 +0200

    remove expense from database

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -6,7 +6,8 @@ import {
     setExpenses,
     removeExpense,
     editExpense,
-    startSetExpenses
+    startSetExpenses,
+    startRemoveExpense
 } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
@@ -29,6 +30,25 @@ test('remove expense', () => {
     expect(action).toEqual({
         type: 'REMOVE_EXPENSE',
         id: '123abc'
+    });
+});
+
+test('remove expense from database', (done) => {
+    const store = createMockStore({});
+    const id = expenses[0].id;
+
+    store.dispatch(startRemoveExpense({ id })).then(() => {
+        const actions = store.getActions();
+
+        expect(actions[0]).toEqual({
+            type: 'REMOVE_EXPENSE',
+            id
+        });
+
+        return database.ref(`expenses/${id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val()).toEqual(null);
+        done();
     });
 });
 
